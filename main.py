7ac2bcbb5a3f701852ebcd38f4b06b9a47a2bc62
krpff <misha@kurapov.ru>
2023-07-03 01:56:05 +0300

    reformat main.py by black

--- main.py
+++ main.py
@@ -7,30 +7,41 @@ headers = {
 
 
 def itmoparse():
-    dir_info = requests.get("https://abitlk.itmo.ru/api/v1/9e2eee80b266b31c8d65f1dd3992fa26eb8b4c118ca9633550889a8ff2cac429/rating/directions?degree=bachelor", headers).json()
+    dir_info = requests.get(
+        "https://abitlk.itmo.ru/api/v1/9e2eee80b266b31c8d65f1dd3992fa26eb8b4c118ca9633550889a8ff2cac429/rating/directions?degree=bachelor",
+        headers,
+    ).json()
     directions = {}
     abiturients = {}
     print("Parsing started")
     for dir in dir_info["result"]["items"]:
-        dir_list = requests.get(f"https://abitlk.itmo.ru/api/v1/rating/bachelor/budget?program_id={dir['isu_id']}&manager_key=&sort=&showLosers=true", headers).json()["result"]
+        dir_list = requests.get(
+            f"https://abitlk.itmo.ru/api/v1/rating/bachelor/budget?program_id={dir['isu_id']}&manager_key=&sort=&showLosers=true",
+            headers,
+        ).json()["result"]
         directions[dir["direction_title"]] = []
         print("Processing", dir["direction_title"])
-        for group in ["without_entry_tests", "by_unusual_quota", "by_special_quota", "by_target_quota", "general_competition"]:
+        for group in [
+            "without_entry_tests",
+            "by_unusual_quota",
+            "by_special_quota",
+            "by_target_quota",
+            "general_competition",
+        ]:
             if dir_list[group]:
                 for abit in dir_list[group]:
-                    directions[dir["direction_title"]].append((abit["snils"], abit["case_number"], abit["total_scores"]))
+                    directions[dir["direction_title"]].append(
+                        (abit["snils"], abit["case_number"], abit["total_scores"])
+                    )
 
         for abit in directions[dir["direction_title"]]:
             id = abit[0] if abit[0] else abit[1]
             if id not in abiturients.keys():
-                abiturients[id] = {
-                    "score": abit[2],
-                    "directions": []
-                }
+                abiturients[id] = {"score": abit[2], "directions": []}
             abiturients[id]["directions"].append(dir["direction_title"])
 
     print("Creating file")
-    itmo_abiturients = xlsxwriter.Workbook('itmo_abiturients.xlsx')
+    itmo_abiturients = xlsxwriter.Workbook("itmo_abiturients.xlsx")
     worksheet = itmo_abiturients.add_worksheet()
     worksheet.write_row(0, 0, ["СНИЛС/Дело", "Балл", "Направления"])
     for i, abit_id in enumerate(abiturients.keys()):
@@ -39,5 +50,6 @@ def itmoparse():
         worksheet.write_row(i + 1, 2, abiturients[abit_id]["directions"])
     itmo_abiturients.close()
     print("File saved as 'itmo_abiturients.xlsx'")
+
 
 itmoparse()
